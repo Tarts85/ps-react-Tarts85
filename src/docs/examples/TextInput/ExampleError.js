8ff61cef9abdb6538a29461b38f0ebbc05539926
Tarts85 <tarmo.kiik@gmail.com>
2020-04-30 00:22:10 +0300

    Created TextInput molecule

---
+++ src/docs/examples/TextInput/ExampleError.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import TextInput from 'ps-react-tarts85/TextInput';
+
+/** Required TextBox with error */
+export default class ExampleError extends Component {
+    render() {
+        return (
+            <TextInput
+                htmlId="example-error"
+                label="First name"
+                name="firstname"
+                onChange={() => {}}
+                required
+                error="First name is required."
+            />
+        )
+    }
+}
---
+++ src/docs/examples/TextInput/ExampleOptional.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TextInput from 'ps-react-tarts85/TextInput';
+
+/** Optional TextBox */
+export default class ExampleOptional extends Component {
+    render() {
+        return (
+            <TextInput
+                htmlId="example-optional"
+                label="First name"
+                name="firstname"
+                onChange={() => {}}
+            />
+        )
+    }
+}
